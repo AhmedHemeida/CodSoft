3f69de67e750cf49c9530cbdcdd9760b5490d122
AhmedHemeida <aelhosary787@gmail.com>
2023-10-09 00:57:22 +0300

    ln

--- Job-Website/BackEnd/app.js
+++ Job-Website/BackEnd/app.js
@@ -5,8 +5,8 @@ const port=process.env.PORT||8000 ;
 
 
 app.use((req, res, next) => {
+  
   res.setHeader('Access-Control-Allow-Origin', 'https://job-three-chi.vercel.app'); 
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
